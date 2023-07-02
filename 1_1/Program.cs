2878f2b269c3fdaf5889546ff68ac4d422257edc
Olga Badzilouskaja <olga2978788@yandex.ru>
2023-07-02 18:56:38 +0300

    1_11

---
+++ 1_1/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World8888888888!");
+Console.WriteLine("Hello, World7777777777!");
+Console.WriteLine("Hello, World66666666666!");
